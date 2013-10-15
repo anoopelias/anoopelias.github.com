c75c57e7e7264b0f105f35c351be318c9b7ecf7c
Anoop Elias <anoopelias@gmail.com>
2013-10-16 00:56:31 +0530

    Added fetching values

--- js/arrangeDemo.js
+++ js/arrangeDemo.js
@@ -1,21 +1,50 @@
 
 (function($) {
     var surface;
+    var pn;
+    var cn;
     $(document).ready( function() {
         surface = SVG('surface').size(600, 500);
+        $('#gen').click(genRandom);
 
         var p = {};
         p.x = 70;
         p.y = 70;
-        plot(p);
+        plotpoint(p);
         
         p.x = 300;
         p.y = 270;
-        plot(p);
+        plotpoint(p);
     });
 
-    var plot = function(p) {
+    var plotpoint = function(p) {
         surface.circle(6).attr({fill: '#000'}).move(p.x, p.y);
+    };
+
+    var genRandom = function(e) {
+        if(validate()) {
+            console.log('validation pass');
+            
+        } else {
+            console.log('validation fail');
+        }
+    };
+
+    var validate = function() {
+        pn = +($('#pn').val());
+        cn = +($('#cn').val());
+
+        if(isNaN(pn))
+            return false;
+        if(isNaN(cn))
+            return false;
+        
+        if(pn <=0)
+            return false;
+        if(cn <=0 || cn > (pn * pn))
+            return false;
+
+        return true;
     };
 
 }(jQuery));
